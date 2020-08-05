3006fef7c00464b3f6c623a1e583d8fad1b3e0bf
Pedro Rambo <henrique.pr92@gmail.com>
2020-08-05 12:49:40 -0300

    Added input component

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -4,24 +4,16 @@ import "./styles.css";
 
 import PageHeader from "../../components/PageHeader";
 import TeacherItem from "../../components/TeacherItem";
+import Input from "../../components/Input";
 
 function TeacherList() {
     return (
         <div id="page-teacher-list">
             <PageHeader title="Estes são os proffys disponíveis">
                 <form id="search-teachers">
-                    <div className="input-block">
-                        <label htmlFor="subject">Matéria</label>
-                        <input type="text" id="subject" />
-                    </div>
-                    <div className="input-block">
-                        <label htmlFor="week_day">Dia</label>
-                        <input type="text" id="week_day" />
-                    </div>
-                    <div className="input-block">
-                        <label htmlFor="time">Hora</label>
-                        <input type="text" id="time" />
-                    </div>
+                    <Input name="subject" label="Matéria"/>
+                    <Input name="week_day" label="Dia"/>
+                    <Input type="time" name="time" label="Hora"/>
                 </form>
             </PageHeader>
 
