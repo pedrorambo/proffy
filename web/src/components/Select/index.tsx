5b963dfa62624b226eb75d467885b7719357d444
Pedro Rambo <henrique.pr92@gmail.com>
2020-08-05 14:04:42 -0300

    Updated default value

--- web/src/components/Select/index.tsx
+++ web/src/components/Select/index.tsx
@@ -15,8 +15,8 @@ const Select: React.FC<SelectProps> = ({label, name, options, ...rest}) => {
     return (
         <div className="select-block">
             <label htmlFor={name}>{label}</label>
-            <select id={name} {...rest}>
-                <option value="" disabled hidden selected>Selecione uma opção</option>
+            <select defaultValue="" id={name} {...rest}>
+                <option value="" disabled hidden>Selecione uma opção</option>
                 {options.map(option => <option key={option.value} value={option.value}>{option.label}</option>)}
             </select>
         </div>
