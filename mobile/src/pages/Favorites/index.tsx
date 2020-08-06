9be8c8350256b6263bfeee935669fb3c46cbee79
Pedro Rambo <henrique.pr92@gmail.com>
2020-08-06 15:13:05 -0300

    Added header

--- mobile/src/pages/Favorites/index.tsx
+++ mobile/src/pages/Favorites/index.tsx
@@ -2,11 +2,12 @@ import React from 'react'
 import { View } from 'react-native';
 
 import styles from './styles';
+import PageHeader from '../../components/PageHeader';
 
 function Favorites(){
     return(
         <View style={styles.container}>
-
+            <PageHeader title="Meus proffys favoritos"></PageHeader>
         </View>
     );
 }
