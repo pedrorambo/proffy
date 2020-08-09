343cb63b6e522e2e46f56df833129ee0c615e79b
Pedro Rambo <henrique.pr92@gmail.com>
2020-08-09 08:25:01 -0300

    Added axios. Connected landing page to API

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {View, Image, Text, TouchableOpacity} from 'react-native';
 import styles from './styles';
 import {useNavigation} from "@react-navigation/native";
@@ -8,6 +8,7 @@ import landingImg from '../../assets/images/landing.png';
 import studyIcon from '../../assets/images/icons/study.png';
 import giveClassesIcon from '../../assets/images/icons/give-classes.png';
 import heartIcon from '../../assets/images/icons/heart.png';
+import api from '../../services/api';
 
 function Landing(){
     const {navigate} = useNavigation();
@@ -19,6 +20,15 @@ function Landing(){
     function handleNavigateToStudyPages(){
         navigate("Study");
     }
+
+    const [totalConnections, setTotalConnections] = useState(0);
+
+    useEffect(() => {
+        api.get("connections").then(response => {
+            const {total} = response.data;
+            setTotalConnections(total);
+        })
+    }, []);
 
     return (
         <View style={styles.container}>
@@ -50,7 +60,7 @@ function Landing(){
             </View>
 
             <Text style={styles.totalConnections}>
-                Total de 285 conexões já realizadas {" "}
+                Total de {totalConnections} conexões já realizadas {" "}
                 <Image source={heartIcon}/>
             </Text>
         </View>
